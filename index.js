a6247b7519cd6852b6a0808fb69ba7bce155dd8c
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-11-14 22:54:31 +0700

    feat: comments

--- index.js
+++ index.js
@@ -50,6 +50,18 @@ async function getLikeCount(videoID) {
   return results[0][0]['like_count'];  
 }
 
+async function getVideoComments(videoID) {
+  const connection = await getMySQLConnection();
+
+  // // Get the like count.
+  const sql = 'SELECT * FROM video_comments WHERE video_comments.video_id = ? ORDER BY video_comments.date DESC';
+  const results = await connection.promise().query(sql, [videoID])
+  connection.end();
+ 
+  return results[0];  
+}
+
+
 // Returns whether the user liked the video or not.
 async function getUserLike(videoID, userID) {
   const connection = await getMySQLConnection();
@@ -70,6 +82,8 @@ async function handleVideo(socket, info) {
   handleVideoViewCount(info);
 }
 
+// videoID
+// userID
 async function handleVideoViewCount(info) {
   // Data expiry time. 
   // After the interval, the value will get updated.
@@ -141,12 +155,87 @@ async function handleVideoLikeCount(info) {
   io.to(room).emit("like", { 'room':room, 'value':value });
 }
 
+async function getUser(connection, uid) {
+  const sql = "SELECT * FROM users WHERE users.ID = ?"
+  const results = await connection.promise().query(sql, [uid])
+  return results[0][0]
+}
+
+function makePrettyDate(date) {
+  const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+  const dateString = month[date.getMonth()].slice(0, 3) + ' ' + date.getDate() + ', ' + date.getFullYear() + ' at ' + date.toLocaleTimeString('en-US', { hour12: false, hour: "numeric", minute: "numeric"});
+  return dateString;
+}
+
+async function handleVideoGetComment(socket, info) {
+  // Data expiry time. 
+  // After the interval, the value will get updated.
+  const expiryTime = 5;
+
+  console.log("HandleVideoComment, videoID: ", info.videoID);
+
+  // Check whether there is a value cached in the redis.
+  const videoKey = info.videoKey+'-comment-key';
+  var value = await redisClient.get(videoKey);
+
+  console.log("Print handleVideoComment value: ", value);
+  
+  if (value == null) {
+    value = await getVideoComments(info.videoID);
+    console.log("Print handleVideoComment FROM DB value: ", value);
+    
+    const connection = await getMySQLConnection();
+    
+    const newValue = await Promise.all(
+      value.map(async (comment) => {
+        const user = await getUser(connection, comment.user_id);
+        const dateString = makePrettyDate(new Date(comment.date))
+        
+        return {
+          user: user.username,
+          comment: comment.comment,
+          date: dateString,
+        }
+      })
+    )
+    
+    connection.end();
+
+    value = JSON.stringify(newValue);
+
+    console.log("Comment Value: ", value);
+    
+    await redisClient.set(videoKey, value, { EX: 2 });
+  }
+
+  const room = info.videoKey+'room';
+  socket.emit("comment", { 'room':room, 'value':value });
+}
+
+async function handleNewComment(socket, info) {
+  const connection = await getMySQLConnection();
+
+  const user = await getUser(connection, info.user_id);
+  const dateString = makePrettyDate(new Date(info.date))
+  const commentData = {
+    user: user.username,
+    comment: info.comment,
+    date: dateString,
+  }
+  connection.end();
+  console.log("CommentData:", commentData);
+  io.to(info.room).emit("comment", { 'room':info.room, 'value': JSON.stringify([commentData]) });
+}
+
 io.on("connection", (socket) => {
   // Client requesting for view count of a particular video.
   socket.on("video", (info) => handleVideo(socket, info));
   socket.on('getNewViewCount', (info) => handleVideoViewCount(info));
   socket.on('getNewLikeCount', (info) => handleVideoLikeCount(info));
+  socket.on('getVideoComments', (info) => handleVideoGetComment(socket, info));
+  
   socket.on('videoLikeToggle', (info) => handleVideoLikeToggle(info));
+  socket.on('newComment', (info) => handleNewComment(socket, info));
 });
 
 console.log("Started server on port 5001.");
